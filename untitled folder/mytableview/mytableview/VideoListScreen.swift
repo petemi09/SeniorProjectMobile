46fd68b9993b626471d266077f105c23b14ff40c
Mitchell Petellin <petemi09@mitchells-mbp-3.lc.luther.edu>
2019-05-09 00:06:01 -0500

    old stuffss

---
+++ untitled folder/mytableview/mytableview/VideoListScreen.swift
@@ -0,0 +1,24 @@
+//
+//  VideoListScreen.swift
+//  mytableview
+//
+//  Created by Mitchell Petellin on 4/2/19.
+//  Copyright © 2019 Mitchell Petellin. All rights reserved.
+//
+
+import UIKit
+
+class VideoListScreen: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ untitled folder/mytableview/mytableview/VideoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  VideoViewController.swift
+//  mytableview
+//
+//  Created by Mitchell Petellin on 4/2/19.
+//  Copyright © 2019 Mitchell Petellin. All rights reserved.
+//
+
+import UIKit
+
+class VideoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
